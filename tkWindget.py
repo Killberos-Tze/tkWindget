75aaeb5815d3bb8f163472e82aa715bfb07a6a0e
Killberos-Tze <killberos.tze@gmail.com>
2024-01-28 20:34:34 +0100

    work on load files

--- tkWindget.py
+++ tkWindget.py
@@ -6,12 +6,100 @@ Created on Tue Feb 28 07:37:01 2023
 @author: tze
 """
 
-from tkinter import Frame, Button, Label, GROOVE, StringVar, Tk
+from tkinter import Frame, Button, Label, GROOVE, StringVar, Tk, SUNKEN
+from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
 import os
-
 #we need load file element: that is a button with a label where the name of the file is displayed
+#here we need to send the refrerence to error message, stringvar for display, we keep name and dir name of the loaded file, we need to send the command how to read the file, arrangement should always be vertical
+#button, label label text, label with filename
+
 #we need load files and clear files element: two buttons with labellist with sliders
+
+
+class LoadDataFile(Frame):
+    class container():
+        pass
+    
+    def __init__(self,**kwargs):
+        self.filename=StringVar()
+        kwargs=self.process_kwargs(**kwargs)
+        super().__init__(kwargs['parent'])
+        kwargs['parent']=self
+        self.prepare_elements(**kwargs)
+        self.errormsg=kwargs['ErrorVar']
+        #self.init_dir=kwargs['initialdir']
+        self._write_function=kwargs['write_file']
+        self._read_function=kwargs['read_file']
+        self._action_function=kwargs['action']
+        
+    def prepare_elements(self,*args,**kwargs):
+        rowcount=1
+        self.browse=Button(kwargs['parent'], text="Load file", command=lambda kwargs=kwargs : self.get_file(**kwargs))
+        self.browse.grid(row=rowcount,column=1,sticky='W')
+        rowcount+=1
+        self.titlelabel=Label(kwargs['parent'], font='Courier',width=24,text=kwargs['text'],anchor='w')
+        self.titlelabel.grid(row=rowcount,column=1,columnspan=2,sticky='W')
+        rowcount+=1
+        self.filelabel=Label(kwargs['parent'], textvariable=self.filename, font='Courier',width=kwargs['width'], wraplength=240,justify='left',relief=SUNKEN,anchor='w')
+        self.filelabel.grid(row=rowcount,column=1,columnspan=2,sticky='W')
+        
+    def get_file(self,**kwargs):
+        self.errormsg.set('')
+        filename=askopenfilename(title="Select file", initialdir=kwargs['initialdir'], filetypes=kwargs['filetypes'])
+        if filename:#to check if anything has been read out
+            #change the folder where to look for the files
+            tmp=self._read_function(filename)
+            if tmp.error=='':
+                self.filename.set(os.path.basename(filename))
+                self.filedir=os.path.dirname(filename)
+                self._write_function()
+                if tmp.data:
+                    self.data=tmp.data
+                if tmp.setup:
+                    self.setup=tmp.setup
+                self._action_function()
+            else:
+                self.filename.set('')
+                self.errormsg.set(tmp.error)
+        
+    def default_read(self,filename):
+        tmp=LoadDataFile.container()
+        tmp.error='test'
+        tmp.data=''
+        tmp.setup=''
+        return tmp
+    
+    def default_write(self):
+        pass
+    
+    def default_action(self):
+        pass
+    
+    def process_kwargs(self,**kwargs):
+        if 'parent' not in kwargs:
+            kwargs['parent']=None
+        if 'text' not in kwargs:
+            kwargs['text']='Loaded whatever:'
+        if 'read_func' not in kwargs:
+            kwargs['read_file']=self.default_read
+        if 'write_file' not in kwargs:
+            kwargs['write_file']=self.default_write
+        if 'action' not in kwargs:
+            kwargs['action']=self.default_action
+        if 'width' not in kwargs:
+            kwargs['width']=12
+        if 'title' not in kwargs:
+            kwargs['title']='Select file'
+        if 'initialdir' not in kwargs:
+            #initialdir should be a reference to a stringvar from main program
+            kwargs['initialdir']=StringVar()
+            kwargs['initialdir'].set('Documents')
+        if 'ErrorVar' not in kwargs:
+            kwargs['ErrorVar']=self.filename
+        if 'filetypes' not in kwargs:
+            kwargs['filetypes']=[("All files","*.*")]
+        return kwargs
 
 
 class OnOffButton(Frame):
@@ -151,7 +239,7 @@ class AppFrame(Frame):
             kwargs['parent']=Tk()
             self.approot=kwargs['parent']
         if 'appgeometry' not in kwargs:
-            kwargs['appgeometry']=(100,100,10,10)
+            kwargs['appgeometry']=(200,200,10,10)
         return kwargs
 
     def init_start(self):
@@ -161,21 +249,24 @@ class AppFrame(Frame):
         self.approot.mainloop()
 
 
-class Test_GUI():
-    def __init__(self):
-        self.root = Tk()
-        self.root.geometry("900x480")
-        self.root.title("Windgets to see")
-        self.rootframe=Frame(self.root)
-        self.rootframe.pack(pady = (25,25), padx = (25,25))
-        self.rotate=Rotate(parent=self.rootframe,direction='horizontal')
+class Test_GUI(AppFrame):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs,appgeometry=(400, 400, 25, 25))
+        self.approot.title("Windgets to see")
+        self.rotate=Rotate(parent=self.root,direction='horizontal')
         self.rotate.grid(row=0,column=1)
-        self.press=OnOffButton(parent=self.rootframe)
+        self.press=OnOffButton(parent=self.root)
         self.press.enable_press()
         self.press.grid(row=1,column=1)
         
-    def init_start(self):
-        self.root.mainloop()
+        self.loadfile=LoadDataFile(parent=self.root, width=24)
+        self.loadfile._write_function=self.write_file
+        self.loadfile.grid(row=2,column=1)
+        
+        
+    def write_file(self):
+        variable="hey"
+        print(variable)
         
 
 if __name__=='__main__':
